6debed3ca01ba7ec038634f994596c9f41603252
ArmanShaikh05 <arman.shaikh01082003@gmail.com>
2024-03-15 18:24:40 +0530

    delete newspaper bug

--- controllers/newspaperController.js
+++ controllers/newspaperController.js
@@ -11,17 +11,19 @@ export const createNewspaper = async (req, res, next) => {
     const { title } = req.body;
     const response = await uploadPdf(req.file.path);
 
-    await newspaper.create({
-      title,
-      newspaper: {
-        url: response.secure_url,
-        public_id: response.public_id,
-      },
-    });
-    res.json({
-      success: true,
-      message: "News Created Successfully",
-    });
+    if (response) {
+      await newspaper.create({
+        title,
+        newspaper: {
+          url: response.secure_url,
+          public_id: response.public_id,
+        },
+      });
+      res.json({
+        success: true,
+        message: "News Created Successfully",
+      });
+    }
   } catch (error) {
     res.json({
       success: false,
